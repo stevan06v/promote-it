5317c2287c21c193494e80dc4a09d8739d3bb2b7
Stevan Vlajic <stevanvlajic5@gmail.com>
2023-07-06 19:55:06 +0200

    feat: subscribed sub-reddits added to the list

--- reddit/main.py
+++ reddit/main.py
@@ -23,17 +23,31 @@ reddit = praw.Reddit(
     username=USER_NAME,
     password=PASSWORD
 )
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
+
+time.sleep(60 * 30)
 while True:
     with open("post.json", "r") as file:
         json_obj = json.load(file)
 
     popular_subreddits = reddit.subreddits.popular()
+    subscribed_subreddits = [subreddit.display_name.lower() for subreddit in reddit.user.subreddits(limit=None)]
+
+    subscribed_subreddits = [subreddit.display_name.lower() for subreddit in reddit.user.subreddits(limit=None)]
 
     # add all popular subreddit-names to list
     for iterator in popular_subreddits:
         subreddits.append(iterator.display_name.lower())
-
 
     post_title = json_obj["title"]
     post_content = json_obj["content"]
@@ -41,6 +55,7 @@ while True:
 
     # combine subreddits
     subreddits += post_subreddits
+    subreddits += subscribed_subreddits
 
     print("posting...")
 
@@ -48,10 +63,10 @@ while True:
         try:
             subreddit = reddit.subreddit(iterator)
             subreddit.submit(post_title, selftext=post_content)
-            print("Successfully posted affiliate-link to: " + iterator)
+            print(bcolors.OKGREEN + "Successfully posted affiliate-link to: " + iterator + bcolors.ENDC)
         except:
-            print("Could not post to: " + iterator)
+            print(bcolors.FAIL + "Could not post to: " + iterator + bcolors.ENDC)
             subreddits.remove(iterator)
-            
+
     subreddits.clear()
     time.sleep(60 * 10)
